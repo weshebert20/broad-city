a01280f7b123edcf2d8a88922b365afe66dc5cf3
weshebert20 <weshebert20@gmail.com>
2017-09-27 12:07:10 -0600

    cant get click to work

--- js/app.js
+++ js/app.js
@@ -1,7 +1,130 @@
 $(document).ready(function() {
+
+    var cardsPlayed = [];
+    var cards = $('.cards');
+    cards = [
+        {
+            theme: "bingo",
+            images: "img/bingo.jpg",
+        },
+        {
+            theme: "bingo",
+            images: "img/bingo.jpg",
+        },
+        {
+            placement: "upperLeft",
+            images: "img/bingo.jpg"
+        },
+        {
+            placement: "upperLeft",
+            images: "img/bingo.jpg"
+        },
+        {
+            placement: "upperLeft",
+            images: "img/bingo.jpg"
+        },
+        {
+            placement: "upperLeft",
+            images: "img/bingo.jpg"
+        },
+        {
+            placement: "upperLeft",
+            images: "img/bingo.jpg"
+        },
+        {
+            placement: "upperLeft",
+            images: "img/bingo.jpg"
+        },
+        {
+            placement: "upperLeft",
+            images: "img/bingo.jpg"
+        },
+        {
+            placement: "upperLeft",
+            images: "img/bingo.jpg"
+        },
+        {
+            placement: "upperLeft",
+            images: "img/bingo.jpg"
+        },
+        {
+            placement: "upperLeft",
+            images: "img/bingo.jpg"
+        },
+        {
+            placement: "upperLeft",
+            images: "img/bingo.jpg"
+        },
+        {
+            placement: "upperLeft",
+            images: "img/bingo.jpg"
+        },
+        {
+            placement: "upperLeft",
+            images: "img/bingo.jpg"
+        },
+        {
+            placement: "upperLeft",
+            images: "img/bingo.jpg"
+        },
+        {
+            placement: "upperLeft",
+            images: "img/bingo.jpg"
+        },
+        {
+            placement: "upperLeft",
+            images: "img/bingo.jpg"
+        },
+    ];
+
+    // create board for gameplay
+    var createBoard = function(){
+     for (var i = 0; i < cards.length; i++){
+        $('#gameContainer').prepend('<img src="card.jpg" class="cards"/>');
+     }
+    };
+
+    createBoard();
+
+
+    var cardId = $('img.cards').each(function(i){
+        $(this).attr('id', 'img' + i);
+    });
+
+    var cardImg = function(){
+        cardId.each(function(i){
+        $(this).attr('src', cards[i].images);
+        });
+    };
+    // console.log(cardImg());
+
+    cardId.click(cardImg).show();
+
+
+
+    // card flip function
+    // $('img#img' + []).each(function(i) {
+    //     .attr('id', 'img' + i).click(function(){
+    //     $(this).attr('src', cards[i].images).toggle();
+    //     });
+    //     console.log(this);
+    // });
+
+
+
+
+
+
+
+
+
+
+
+
+    // create fadein and out transistions
     $("body").css("display", "none");
  
-    $("body").fadeIn(2000);
+    $("body").fadeIn(1000);
  
     $("a.transition").click(function(event){
         event.preventDefault();
@@ -12,4 +135,5 @@ $(document).ready(function() {
     function redirectPage() {
         window.location = linkLocation;
     }
+
 });
